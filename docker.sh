096964d86669ad77136c097c124766a8755abbfa
ymir07 <83964272+ymir07@users.noreply.github.com>
2024-02-26 14:46:21 +0300

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 sudo install -m 0755 -d /etc/apt/keyrings
-$ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
 sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-$ sudo chmod a+r /etc/apt/keyrings/docker.gpg
-$ echo \
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
   "deb [arch="$(dpkg --print-architecture)" \
 signed-by=/etc/apt/keyrings/docker.gpg] \
 https://download.docker.com/linux/ubuntu \
@@ -11,5 +11,5 @@ https://download.docker.com/linux/ubuntu \
 stable" | sudo tee /etc/apt/sources.list.d/docker.list \
 > /dev/null
 sudo apt-get update
-$ sudo apt-get -y install docker-ce docker-ce-cli \
+sudo apt-get -y install docker-ce docker-ce-cli \
 containerd.io docker-buildx-plugin docker-compose-plugin

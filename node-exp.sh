dfb99c9de610a9401dd09506828139bc9e67a0c3
ymir07 <83964272+ymir07@users.noreply.github.com>
2024-03-02 18:03:18 +0300

    Update and rename node-exp to node-exp.sh

---
+++ node-exp.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+version="${VERSION:-1.7.1}"
+arch="${ARCH:-linux-amd64}"
+bin_dir="${BIN_DIR:-/usr/local/bin}"
+
+wget "https://github.com/prometheus/node_exporter/releases/download/v$version/node_exporter-$version.$arch.tar.gz" \
+    -O /tmp/node_exporter.tar.gz
+
+mkdir -p /tmp/node_exporter
+
+cd /tmp || { echo "ERROR! No /tmp found.."; exit 1; }
+
+tar xfz /tmp/node_exporter.tar.gz -C /tmp/node_exporter || { echo "ERROR! Extracting the node_exporter tar"; exit 1; }
+
+cp "/tmp/node_exporter/node_exporter-$version.$arch/node_exporter" "$bin_dir"
+chown root:staff "$bin_dir/node_exporter"
+
+cat <<EOF > /etc/systemd/system/node_exporter.service
+[Unit]
+Description=Node Exporter
+Wants=network-online.target
+After=network-online.target
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+ExecStart=/usr/local/bin/node_exporter
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl enable node_exporter.service
+systemctl start node_exporter.service
+

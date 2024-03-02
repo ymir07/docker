5a65e2e85ccafa14ff57c45dba2ab8e0513fce07
ymir07 <83964272+ymir07@users.noreply.github.com>
2024-03-02 18:20:16 +0300

    Update node-exp.sh

--- node-exp.sh
+++ node-exp.sh
@@ -13,7 +13,8 @@ cd /tmp || { echo "ERROR! No /tmp found.."; exit 1; }
 tar xfz /tmp/node_exporter.tar.gz -C /tmp/node_exporter || { echo "ERROR! Extracting the node_exporter tar"; exit 1; }
 
 cp "/tmp/node_exporter/node_exporter-$version.$arch/node_exporter" "$bin_dir"
-chown root:staff "$bin_dir/node_exporter"
+useradd --no-create-home --shell /bin/false node_exporter
+chown node_exporter:node_exporter "$bin_dir/node_exporter"
 
 cat <<EOF > /etc/systemd/system/node_exporter.service
 [Unit]
